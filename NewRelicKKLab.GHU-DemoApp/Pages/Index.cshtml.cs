08281430b6670c79a6bddefc6bd6d2f420d25c08
tanaka_733 <tanaka-takayoshi@users.noreply.github.com>
2024-10-14 21:46:16 +0900

    プロジェクト ファイルを追加します。

---
+++ NewRelicKKLab.GHU-DemoApp/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NewRelicKKLab.GHU_DemoApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
