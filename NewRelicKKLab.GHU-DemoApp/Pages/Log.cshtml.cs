c669d753cee644d3715394530a5ae5b20727c402
tanaka_733 <tanaka-takayoshi@users.noreply.github.com>
2024-10-18 22:44:13 +0900

    Add log.

--- NewRelicKKLab.GHU-DemoApp/Pages/Log.cshtml.cs
+++ NewRelicKKLab.GHU-DemoApp/Pages/Log.cshtml.cs
@@ -3,17 +3,22 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace NewRelicKKLab.GHU_DemoApp.Pages
 {
-    public class IndexModel : PageModel
+    public class LogModel : PageModel
     {
+
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public LogModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
         public void OnGet()
         {
+            // Log a message to the console
+            System.Console.WriteLine("This is a console log message");
+            // Log a message to the output window
+            System.Diagnostics.Debug.WriteLine("This is a debug output message");
             _logger.LogInformation("This is an information log message");
         }
     }
